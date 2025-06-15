be508b7e5a3199a5e95daeeb73f2e77a673db1e3
Jobe6 <sbonisosthole9@gmail.com>
2025-06-15 05:50:47 +0200

    Add files via upload

---
+++ LogIn/login.js
@@ -0,0 +1,20 @@
+function verifyUser(){
+    var username = document.getElementById("usernameInput").value;
+    var password = document.getElementById("passwordInput").value;
+
+    checkUserCreds(username, password);
+
+}
+function checkUserCreds(username, password){
+    var systemUsername = "Bond";
+    var systemPassword = "079";
+
+    if(username == systemUsername &&  password == systemPassword){
+            document.getElementById("message").innerHTML
+            = "Correct. Logging you in....";
+        }
+        else{
+            document.getElementById("message").innerHTML
+            = "Incorrect. Try again";
+        }
+}
